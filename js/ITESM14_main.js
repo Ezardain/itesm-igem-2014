1bc684c3bc3a7fb24a1b8f0b56e531db0e9c094e
sebastianjau <sebastianjau@gmail.com>
2014-10-07 23:11:54 -0500

    agregue jquery para el cambio de la imagen del jumbotron

--- js/ITESM14_main.js
+++ js/ITESM14_main.js
@@ -32,4 +32,47 @@ $('#show-menu').on('click', function(e) {
 
 $(function () {
   $('#myTab a:first').tab('show')
-})
+});
+
+$(document).ready(function(){
+  $("#overviewTab").click(function(){
+    $("#notebookheader").show();
+    $("#module1header").hide();
+    document.title = "Overview";
+  });
+  $("#notebookTab").click(function(){
+    $("#notebookheader").hide();
+    $("#module1header").show();
+    document.title = "Notebook";
+  });
+  $("#protocolsTab").click(function(){
+    $("#notebookheader").hide();
+    $("#module1header").show();
+    document.title = "Protocols";
+  });
+  $("#modelingTab").click(function(){
+    $("#notebookheader").hide();
+    $("#module1header").show();
+    document.title = "Modeling";
+  });
+  $("#module1Tab").click(function(){
+    $("#notebookheader").hide();
+    $("#module1header").show();
+    document.title = "Module 1";
+  });
+  $("#module2Tab").click(function(){
+    $("#notebookheader").hide();
+    $("#module1header").show();
+    document.title = "Module 2";
+  });
+  $("#module3Tab").click(function(){
+    $("#notebookheader").hide();
+    $("#module1header").show();
+    document.title = "Module 3";
+  });
+  $("#module4Tab").click(function(){
+    $("#notebookheader").hide();
+    $("#module1header").show();
+    document.title = "Module 4";
+  });
+});
